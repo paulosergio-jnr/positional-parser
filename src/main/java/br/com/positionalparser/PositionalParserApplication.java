fbe8978fdbe3dbb7dc9eb914559d727e2949065c
Paulo Sergio Pereira Junior <paulosergio.jnr@gmail.com>
2020-05-30 15:04:14 -0300

    Initial commit

---
+++ src/main/java/br/com/positionalparser/PositionalParserApplication.java
@@ -0,0 +1,13 @@
+package br.com.positionalparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PositionalParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PositionalParserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/positionalparser/PositionalParserApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.positionalparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PositionalParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
